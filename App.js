86483ee89fa84aed558f11775cd8f93d7d3d6ae1
langochai <langochai11121998@gmail.com>
2024-02-15 14:07:24 +0700

    update to read folder

--- App.js
+++ App.js
@@ -1,67 +1,157 @@
 import React, { useState, useRef, useEffect } from "react";
-import { View, Text, Button } from "react-native";
-import * as DocumentPicker from "expo-document-picker";
+import {
+    View,
+    Text,
+    Button,
+    FlatList,
+    Alert,
+    SafeAreaView,
+} from "react-native";
+import { StorageAccessFramework } from "expo-file-system";
 import { Audio } from "expo-av";
+import { LogBox } from "react-native";
 
 export default function App() {
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [sound, setSound] = useState(null);
-    const [fileName, setFileName] = useState(null);
-
+    LogBox.ignoreAllLogs();
+    const [musicFiles, setMusicFiles] = useState([]);
+    const [currentSong, setCurrentSong] = useState(null);
     const soundObject = useRef(new Audio.Sound());
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
-        return () => {
-            if (sound) {
-                sound.unloadAsync();
+        const loadAudio = async () => {
+            try {
+                if (currentSong) {
+                    soundObject.current.unloadAsync();
+                    await soundObject.current.loadAsync({ uri: currentSong });
+                    setIsLoaded(true);
+                    if (isPlaying) await soundObject.current.playAsync();
+                }
+            } catch (error) {
+                console.log("Error loading audio:", error);
+                Alert.alert(
+                    "Error",
+                    "An error occurred while loading the audio file."
+                );
             }
         };
-    }, []);
-    const pickFile = async () => {
+
+        loadAudio();
+
+        return () => {
+            if (isLoaded) {
+                soundObject.current.stopAsync();
+                soundObject.current.unloadAsync();
+            }
+        };
+    }, [currentSong]);
+
+    const loadMusicFiles = async () => {
         try {
-            const pickerResult = await DocumentPicker.getDocumentAsync({
-                type: "audio/*", // Specify the MIME type for audio files
-            });
-            if (!pickerResult.cancelled) {
-                const uri = pickerResult.assets[0].uri;
-                const name = pickerResult.assets[0].name;
-                setFileName(name);
+            const permissions =
+                await StorageAccessFramework.requestDirectoryPermissionsAsync();
 
-                await soundObject.current.unloadAsync(); // Stop any currently playing sound
-
-                await soundObject.current.loadAsync(
-                    { uri },
-                    { shouldPlay: false }
+            if (permissions.granted) {
+                const uri = permissions.directoryUri;
+                const files = await StorageAccessFramework.readDirectoryAsync(
+                    uri
                 );
-                setSound(soundObject.current);
+
+                const musicFiles = files.filter(
+                    (fileName) =>
+                        fileName.endsWith(".mp3") || fileName.endsWith(".m4a")
+                );
+                setMusicFiles(musicFiles);
+                setIsPlaying(false); // Revert play/pause button to "play"
+            } else {
+                Alert.alert(
+                    "Permission Denied",
+                    "Please grant permission to access files."
+                );
             }
         } catch (error) {
-            console.log("Error picking file:", error);
+            console.log("Error loading music files:", error);
+            Alert.alert(
+                "Error",
+                "An error occurred while loading music files."
+            );
         }
     };
 
     const togglePlayPause = async () => {
-        if (sound) {
-            if (isPlaying) {
-                await soundObject.current.pauseAsync();
-            } else {
-                await soundObject.current.playAsync();
+        try {
+            if (isLoaded) {
+                if (isPlaying) await soundObject.current.pauseAsync();
+                else await soundObject.current.playAsync();
+                setIsPlaying(!isPlaying);
             }
-            setIsPlaying(!isPlaying);
+        } catch (error) {
+            console.log("Error toggling play/pause:", error);
+            Alert.alert(
+                "Error",
+                "An error occurred while toggling play/pause."
+            );
+        }
+    };
+
+    const playMusicFile = async (fileName) => {
+        try {
+            setCurrentSong(fileName);
+            setIsPlaying(true);
+        } catch (error) {
+            console.log("Error playing music file:", error);
+            Alert.alert(
+                "Error",
+                "An error occurred while playing the music file."
+            );
         }
     };
 
     return (
         <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                marginHorizontal: "2"
+            }}
         >
-            <Text>File Name: {fileName}</Text>
-            <Button title="Pick File" onPress={pickFile} />
-            <Button
-                title={isPlaying ? "Pause" : "Play"}
-                onPress={togglePlayPause}
-                disabled={!sound}
+            <Text>Music Files:</Text>
+            <FlatList
+                data={musicFiles}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Text>{extractFileName(item)}</Text>
+                        <Button
+                            title={
+                                item === currentSong && isPlaying
+                                    ? "Pause"
+                                    : "Play"
+                            }
+                            onPress={() => {
+                                if (item === currentSong) togglePlayPause();
+                                else playMusicFile(item);
+                            }}
+                        />
+                    </View>
+                )}
             />
+            <Button title="Choose Folder" onPress={loadMusicFiles} />
         </View>
     );
 }
+
+const extractFileName = (uri) => {
+    const decodedUri = decodeURIComponent(uri);
+    const parts = decodedUri.split("/");
+    const encodedFileName = parts[parts.length - 1];
+    const decodedFileName = decodeURIComponent(encodedFileName);
+    return decodedFileName;
+};
